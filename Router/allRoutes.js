5cf628c9a11ce6c71404cf64eda875edb56e2160
Sephora-p <pietrussephora@gmail.com>
2025-05-17 17:48:59 +0200

    Création de nouvelles pages

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,9 @@ import Route from "./Route.js";
 export const allRoutes = [
 
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("Covoiturage", "Covoiturage", "/pages/Covoiturage.html"),
+    new Route("/Covoiturage", "Covoiturage", "/pages/Covoiturage.html"),
+    new Route("/Connexion", "Connexion", "/pages/Connexion.html"),
+    new Route("/Contact", "Contact", "/pages/Contact.html"),
     ];
 
     //Le titre s'affiche comme ceci : Route.titre - websitename
