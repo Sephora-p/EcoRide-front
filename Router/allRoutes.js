0e414d81ecb82e3bd94e570e34a8fd9cbe03a449
Sephora-p <pietrussephora@gmail.com>
2025-05-19 20:08:53 +0200

    modification fichier auth

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,10 +4,10 @@ export const allRoutes = [
 
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html"),
-  new Route("/signin", "Connexion", "/pages/authentification/signin.html"),
-  new Route("/signup", "Inscription", "/pages/authentification/signup.html"),
-  new Route("/compte", "Mon compte", "/pages/authentification/compte.html"),
-  new Route("/editPassword", "changement de mot de passe", "/pages/authentification/editPassword.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/compte", "Mon compte", "/pages/auth/compte.html"),
+  new Route("/password", "Réinitialiser", "/pages/auth/password.html"),
   new Route("/allResa", "Ma réservation", "/pages/reservation/allResa.html"),
   new Route("/contact", "Contact", "/pages/contact.html"),
 ];
